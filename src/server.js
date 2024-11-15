dbf262d8ab91b2facdc6399d3fa7fb2ebe899ac7
kilyfa <122576090+kilyfa@users.noreply.github.com>
2024-11-16 06:48:24 +0700

    update

--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
-const hapi = require('@hapi/hapi');
-const routes = require('./route');
+const hapi = require("@hapi/hapi");
+const routes = require("./route");
 
 const init = async () => {
-    const server = hapi.Server({
-        port: 9000,
-        host: 'localhost',
-    });
+  const server = hapi.Server({
+    port: process.env.PORT || 8000,
+    host: "0.0.0.0",
+  });
 
-    server.route(routes);
+  server.route(routes);
 
-    await server.start();
-    console.log(`server sedang berjalan di ${server.info.uri}`);
+  await server.start();
+  console.log(`server sedang berjalan di ${server.info.uri}`);
 };
 
 init();
